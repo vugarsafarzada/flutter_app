362918593873e546cbc3efc9334142f4d61fc8d8
Vugar Safarzada <vuqarsfrzad@gmail.com>
2023-08-28 16:43:38 +0400

    dark theme active switch

---
+++ lib/MyApp.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'assets/colors.dart';
+
+class HelloApp extends StatelessWidget{
+  final bool darkThemeActive;
+  const HelloApp({Key? key, required this.darkThemeActive}) : super(key: key);
+  @override
+  Widget build(BuildContext context){
+    if(!darkThemeActive){
+      return MaterialApp(
+        title: 'MaterialApp for Hello',
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Welcome back!', textDirection: TextDirection.ltr),
+            backgroundColor: specific_colors['black'],
+          ),
+          body: const Center(
+            child: Text(
+              'Welcome back \nVugar!',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  textBaseline: TextBaseline.alphabetic
+              ),
+              textAlign: TextAlign.center,
+            ),
+
+          ),
+        ),
+      );
+    }
+    return MaterialApp(
+      title: 'MaterialApp for Hello',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome back!', textDirection: TextDirection.ltr),
+          backgroundColor: specific_colors['black'],
+        ),
+        body: const Center(
+          child: Text(
+            'Welcome back \nVugar!',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                textBaseline: TextBaseline.alphabetic
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        backgroundColor: specific_colors['dark'],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:hello/assets/colors.dart';
+import 'MyApp.dart';
 
 void main(){
-  final app;
-  app = HelloApp(darkThemeActive: true);
+  final app = HelloApp(darkThemeActive: false);
   runApp(app);
-}
-
-class HelloApp extends StatelessWidget{
-  final bool darkThemeActive;
-  const HelloApp({Key? key, required this.darkThemeActive}) : super(key: key);
-  @override
-  Widget build(BuildContext context){
-    if(!darkThemeActive){
-      return MaterialApp(
-        title: 'MaterialApp for Hello',
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Welcome back!', textDirection: TextDirection.ltr),
-            backgroundColor: specific_colors['black'],
-          ),
-          body: const Center(
-            child: Text(
-              'Welcome back \nVugar!',
-              textDirection: TextDirection.ltr,
-              style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  textBaseline: TextBaseline.alphabetic
-              ),
-              textAlign: TextAlign.center,
-            ),
-
-          ),
-        ),
-      );
-    }
-    return MaterialApp(
-      title: 'MaterialApp for Hello',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome back!', textDirection: TextDirection.ltr),
-          backgroundColor: specific_colors['black'],
-        ),
-        body: const Center(
-          child: Text(
-            'Welcome back \nVugar!',
-            textDirection: TextDirection.ltr,
-            style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                textBaseline: TextBaseline.alphabetic
-            ),
-            textAlign: TextAlign.center,
-          ),
-        ),
-        backgroundColor: specific_colors['dark'],
-      ),
-    );
-  }
 }
