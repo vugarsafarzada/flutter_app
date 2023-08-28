24b9411111dcc86517039def4bd820a674f02fdc
Vugar Safarzada <vuqarsfrzad@gmail.com>
2023-08-28 16:37:02 +0400

    dark theme active switch

---
+++ lib/assets/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+final Map <String, Color> specific_colors= {
+  'tomato' : const Color.fromRGBO(205, 25, 25, 1),
+  'orange' : const Color.fromRGBO(225, 125, 15, 1),
+  'black' :  const Color.fromRGBO(1, 1, 1, 1),
+  'dark' :  const Color.fromRGBO(100, 100, 100, 1),
+};
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,64 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
+import 'package:hello/assets/colors.dart';
 
 void main(){
-  const app = AppInterface();
+  final app;
+  app = HelloApp(darkThemeActive: true);
   runApp(app);
 }
 
-class AppInterface extends StatelessWidget{
-  const AppInterface({super.key});
+class HelloApp extends StatelessWidget{
+  final bool darkThemeActive;
+  const HelloApp({Key? key, required this.darkThemeActive}) : super(key: key);
   @override
   Widget build(BuildContext context){
-    return MaterialApp(
-      title: 'Material App title',
-      home: Scaffold(
-        appBar: AppBar( title: const Text('Welcome back!', textDirection: TextDirection.ltr)),
-        body: const Center(
-          child: Text('Hello Vugar Safarzada', textDirection: TextDirection.ltr),
+    if(!darkThemeActive){
+      return MaterialApp(
+        title: 'MaterialApp for Hello',
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Welcome back!', textDirection: TextDirection.ltr),
+            backgroundColor: specific_colors['black'],
+          ),
+          body: const Center(
+            child: Text(
+              'Welcome back \nVugar!',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  textBaseline: TextBaseline.alphabetic
+              ),
+              textAlign: TextAlign.center,
+            ),
+
+          ),
         ),
-        backgroundColor: const Color.fromRGBO(126, 126, 126, 1),
+      );
+    }
+    return MaterialApp(
+      title: 'MaterialApp for Hello',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome back!', textDirection: TextDirection.ltr),
+          backgroundColor: specific_colors['black'],
+        ),
+        body: const Center(
+          child: Text(
+            'Welcome back \nVugar!',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                textBaseline: TextBaseline.alphabetic
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        backgroundColor: specific_colors['dark'],
       ),
-    );;
+    );
   }
 }
